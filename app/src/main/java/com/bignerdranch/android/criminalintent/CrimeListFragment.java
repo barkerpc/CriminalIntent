2702138d13b8f4644d4c444826857db881a1d17e
Paul Barker <barkerpc@gmail.com>
2016-04-11 21:03:54 -0400

    Added CrimeListActivity

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Paul Barker on 4/11/2016.
+ */
+public class CrimeListFragment extends Fragment {
+}
