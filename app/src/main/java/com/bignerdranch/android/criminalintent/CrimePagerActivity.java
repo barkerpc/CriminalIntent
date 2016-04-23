0b39724f7f33f4a1091dcbf337cfd3e94ac96794
Paul Barker <barkerpc@gmail.com>
2016-04-23 19:49:46 -0400

    initial commit viewpager

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,56 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by Paul Barker on 4/23/2016.
+ */
+public class CrimePagerActivity extends FragmentActivity {
+
+    private static final String EXTRA_CRIME_ID =
+            "com.bignerdranch.android.criminalintent.crime_id";
+
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+
+    public static Intent newIntent(Context packageContext, UUID crimeID) {
+        Intent intent = new Intent(packageContext,CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID, crimeID);
+        return intent;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        mViewPager = (ViewPager)findViewById(R.id.activity_crime_pager_view_pager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentPagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+
+    }
+}
